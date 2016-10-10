512dc02fbc2195d5bc3e420c237e6e32745aaab1
Ilshidur <nicolas-coutin@outlook.com>
2016-10-10 18:00:55 +0200

    Demo deploy script.

---
+++ demo/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Required :
+# * pm2
+#
+
+cd `dirname $0`
+
+printf "`date +%Y-%m-%d` : Trying ...\n" >> deploy.log
+
+#PM2
+echo "Cheking PM2 ..."
+if npm list -g --depth=0 | grep pm2
+then
+	echo "PM2 already installed."
+else
+	echo "Installing PM2 ..."
+	npm install pm2 -g
+	echo "Installed PM2 !"
+fi
+
+## Install dependencies
+echo "Installing dependencies ..."
+npm install
+echo "Installed dependencies !"
+
+## Run with pm2
+echo "Running PM2 ..."
+pm2 startOrRestart ecosystem.json5
+
+echo "Deployed successfully !"
+printf "`date +%Y-%m-%d` : \n$(git show --name-status)\n\n" >> deploy.log
