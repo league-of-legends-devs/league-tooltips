42f65b25cc8687824c3b3740bb77b256c23e52c1
Ilshidur <maar-gan@hotmail.fr>
2017-06-25 01:47:38 +0200

    Adapted usage according to #3 fix

--- demo/app.js
+++ demo/app.js
@@ -6,6 +6,6 @@ var config = require('./config.js');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(leagueTips(config.key.riot, 'euw', { url: '/tooltips' }));
+app.use('/tooltips', leagueTips(config.key.riot, 'euw'));
 
 app.listen(config.port);
